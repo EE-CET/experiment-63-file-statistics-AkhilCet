5a02d236d856d62668b8db572dfa2d981bf18c3a
AkhilCet <tve24el005@cet.ac.in>
2026-02-28 22:57:14 +0530

    Update FileStats.java

--- src/FileStats.java
+++ src/FileStats.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class FileStats {
     public static void main(String[] args) {
-        String fileName = "sample.txt";
+        String fileName = "experiment-63-file-statistics-AkhilCet\\sample.txt";
         
         int lineCount = 0;
         int wordCount = 0;
@@ -17,14 +17,17 @@ public class FileStats {
             // TODO: Read the file line by line until it returns null
             while ((line = reader.readLine()) != null) {
                 // TODO: Increment lineCount
+                lineCount++;
                 
                 // TODO: Add the length of the current line to charCount
+                charCount += line.length();
                 
                 // TODO: Split the line into words using split("\\s+") and add the length of the resulting array to wordCount
                 // Hint: Check if the line is not empty before splitting to avoid counting empty strings!
                 if (!line.trim().isEmpty()) {
                     String[] words = line.trim().split("\\s+");
                     // Add words.length to wordCount
+                    wordCount += words.length;
                 }
             }
             
